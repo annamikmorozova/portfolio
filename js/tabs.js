05dfcd862949e96b2e1f3ee9c2bb21372b3e4958
Anna Morozova <annamorozova@Annas-Air.fios-router.home>
2020-02-16 23:03:51 -0500

    Add initial files

---
+++ js/tabs.js
@@ -0,0 +1,16 @@
+function tab_click(element) {
+  var tabs = document.getElementsByClassName("tab-section");
+  for (var i = 0; i < tabs.length; i++) {
+    if (element.name == tabs[i].id) {
+      tabs[i].className = "tab-section";
+    } else {
+      tabs[i].className = "tab-section hidden";
+    }
+  }
+
+  var nav_items = document.getElementsByClassName("menuitem");
+  for (var i = 0; i < nav_items.length; i++) {
+    nav_items[i].className = "menuitem nav-link";
+  }
+  element.className = "menuitem nav-link active";
+}
